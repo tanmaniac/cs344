d5cf487286b04a41cd4be5addd4b553efc2832e7
Tanmay Bangalore <tanmaybangalore@gmail.com>
2018-02-02 18:22:09 -0500

    WIP: Non-working min-max kernelTrying to compute min and max of an input array using interleaved indices. Does not work yet, do not merge. Just need to commit so I can access it from another machine.

--- Problem Sets/Problem Set 3/student_func.cu
+++ Problem Sets/Problem Set 3/student_func.cu
@@ -6,7 +6,7 @@
 
   A High Dynamic Range (HDR) image contains a wider variation of intensity
   and color than is allowed by the RGB format with 1 byte per channel that we
-  have used in the previous assignment.  
+  have used in the previous assignment.
 
   To store this extra information we use single precision floating point for
   each channel.  This allows for an extremely wide range of intensity values.
@@ -53,7 +53,7 @@
   Old TV signals used to be transmitted in this way so that black & white
   televisions could display the luminance channel while color televisions would
   display all three of the channels.
-  
+
 
   Tone-mapping
   ============
@@ -81,24 +81,71 @@
 
 #include "utils.h"
 
+__device__ inline int getPosition() {
+    return blockIdx.x * blockDim.x + threadIdx.x;
+}
+
+__device__ inline boundsClamp(const int threadPos, const int offset, const size_t size) {
+    return threadPos + offset >= size ? threadPos : threadPos + offset;
+}
+
+template <typename T>
+__global__ void minMaxKernel(T* d_in, size_t dataSize, T* d_out) {
+  static_assert(std::is_arithmetic<T>::value, "Only arithmetic types are supported");
+    const int threadPos = getPosition();
+    const int threadId = threadIdx.x;
+
+    // Reduce this block
+    for (unsigned int s = blockDim.x >> 1; s > 0; s >>= 1) {
+        if (threadId < s) {
+            // Make sure we don't exceed the bounds of the array. If this thread's comparison index
+            // is outside the array bounds, just compare against itself.
+            int compareIndex = threadPos + s >= size ? threadPos : threadPos + offset;
+            d_in[threadPos].x = min(d_in[threadPos].x, d_in[compareIndex].x);
+            d_in[threadPos].y = max(d_in[threadPos].y, d_in[compareIndex].y);
+        }
+        __syncthreads();
+    }
+
+    // Only write to output array with thread 0
+    if (threadId == 0) {
+        d_out[blockIdx.x] = d_in[threadPos];
+    }
+}
+
+void launchMinMaxKernel(float* d_logLuminance, float& min_logLum, float& max_logLum, const size_t dataSize) {
+    const size_t dataBytes = dataSize * sizeof (float2);
+
+}
+
 void your_histogram_and_prefixsum(const float* const d_logLuminance,
                                   unsigned int* const d_cdf,
-                                  float &min_logLum,
-                                  float &max_logLum,
+                                  float& min_logLum,
+                                  float& max_logLum,
                                   const size_t numRows,
                                   const size_t numCols,
-                                  const size_t numBins)
-{
-  //TODO
-  /*Here are the steps you need to implement
-    1) find the minimum and maximum value in the input logLuminance channel
-       store in min_logLum and max_logLum
-    2) subtract them to find the range
-    3) generate a histogram of all the values in the logLuminance channel using
-       the formula: bin = (lum[i] - lumMin) / lumRange * numBins
-    4) Perform an exclusive scan (prefix sum) on the histogram to get
-       the cumulative distribution of luminance values (this should go in the
-       incoming d_cdf pointer which already has been allocated for you)       */
+                                  const size_t numBins) {
+    // TODO
+    /*Here are the steps you need to implement
+      1) find the minimum and maximum value in the input logLuminance channel
+         store in min_logLum and max_logLum
+    */
+    const int maxThreadsPerBlock = 1024;
+    const size_t dataSize = numRows * numCols;
+    int threads = maxThreadsPerBlock;
+    int blocks = dataSize / maxThreadsPerBlock;
+
+    // Using float2 for shared memory so we can interleave min and max values
+    const size_t shmSize = dataSize * sizeof(float2);
+    minMaxKernel<<<blocks, threads, shmSize>>>(d)
 
 
+    // 2) subtract them to find the range
+    /*3) generate a histogram of all the values in the logLuminance channel using
+         the formula: bin = (lum[i] - lumMin) / lumRange * numBins
+    */
+    /*4) Perform an exclusive scan (prefix sum) on the histogram to get
+         the cumulative distribution of luminance values (this should go in the
+         incoming d_cdf pointer which already has been allocated for you)
+    */
 }
---
+++ test/ps3/test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+TEST(ProblemSet3, AddNumbers)
+{
+    int a = 10;
+    int b = 20;
+    ASSERT_EQ(a + b, 30);
+}
